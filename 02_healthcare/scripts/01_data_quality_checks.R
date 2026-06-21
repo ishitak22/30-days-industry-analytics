384a2cd0bfe26700288977887b2d11ec1972349c
Ishita Khanna <ishitakhanna2208@gmail.com>
2026-06-22 01:41:59 +1000

    did a duplicate search

--- 02_healthcare/scripts/01_data_quality_checks.R
+++ 02_healthcare/scripts/01_data_quality_checks.R
@@ -24,3 +24,12 @@ missing_summary
 missing_summary_sorted <- sort(unlist(missing_summary), decreasing = TRUE)
 
 missing_summary_sorted
+
+# Duplicates
+duplicate_count <- sum(duplicated(elective_surgery))
+
+duplicate_count
+
+duplicate_rows <- janitor::get_dupes(elective_surgery)
+
+duplicate_rows
